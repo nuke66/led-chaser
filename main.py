454f9b5e37397a4f1957b70b06a93b362f356804
Slade Harker <slade_harker@hotmail.com>
2019-06-26 18:31:09 +1000

    Initial PR

---
+++ main.py
@@ -0,0 +1,191 @@
+# Write your code here :-)
+
+import board
+import neopixel
+import time
+
+from npchaser import *
+from npchasery import *
+
+# fix this!  need to put all this into classes
+NUMPIXELS = 16
+neo = neopixel.NeoPixel(board.D5, NUMPIXELS, brightness=0.02, auto_write=False)
+
+
+RED = (255,0,0)
+YELLOW = (255,255,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+PURPLE = (153,0,153)
+WHITE = (255,255,255)
+PINK = (255,51,153)
+
+
+
+
+
+def run():
+    print("lightshow()")
+    neo.fill(0)
+    neo.show()
+    
+def npchaser_test(): 
+    print("Chaser test ***\n") 
+ 
+    #sweep_and_stack_CW(YELLOW,BLUE,0,10,0.01)
+    #time.sleep(1)
+    #sweep(color=RED,bgcolor=0,back=False,delay=0.05,start=0,end=NUMPIXELS-1)
+    sweep(neo,BLUE,RED,False,0.05,0,7)
+    
+    for n in range(1,5):
+        one_in_n(neo,PINK,0,2,0.3)
+    for n in range(1,5):
+        one_in_n(neo,RED,YELLOW,2,0.3)
+    for n in range(1,5):
+        one_in_n(neo,PINK,0,4,0.3)
+    for n in range(1,5):
+        one_in_n(neo,RED,BLUE,4,0.3)    
+    time.sleep(2)
+    sweep_and_stack(neo,YELLOW)
+    sweep_and_stack(neo,GREEN,RED)
+    neo.fill(0)
+    neo.show()
+    sweep_fill_range(neo,RED,0,3)
+    sweep_fill_range(neo,YELLOW,4,7)
+    sweep_fill_range(neo,GREEN,8,11)
+    sweep_fill_range(neo,BLUE,12,15)
+
+    sweep_fill_range(neo,PURPLE,0,3)
+    sweep_fill_range(neo,WHITE,4,7)
+    sweep_fill_range(neo,PINK,8,11)
+    sweep_fill_range(neo,RED,12,15)
+
+    sweep_fill_range(neo,YELLOW,0,3)
+    sweep_fill_range(neo,GREEN,4,7)
+    sweep_fill_range(neo,BLUE,8,11)
+    sweep_fill_range(neo,PURPLE,12,15)
+
+    sweep_fill_range(neo,WHITE,0,3)
+    sweep_fill_range(neo,PINK,4,7)
+    sweep_fill_range(neo,RED,8,11)
+    sweep_fill_range(neo,YELLOW,12,15)
+
+
+
+
+    time.sleep(2)
+    neo.fill(0)
+    neo.show()
+    
+    fill_range(neo,BLUE,0,3)
+    time.sleep(0.5)
+    fill_range(neo,GREEN,4,7)
+    time.sleep(0.5)
+    fill_range(neo,RED,8,11)
+    time.sleep(0.5)
+    fill_range(neo,YELLOW,12,15)
+    time.sleep(0.5)
+    
+    for n in range(1,7):
+          
+        # move 1
+        fill_range(neo,YELLOW,0,3)
+        fill_range(neo,BLUE,4,7)
+        fill_range(neo,GREEN,8,11)
+        fill_range(neo,RED,12,15)
+        time.sleep(0.3)
+    
+        #MOVE 2
+        fill_range(neo,RED,0,3)
+        fill_range(neo,YELLOW,4,7)
+        fill_range(neo,BLUE,8,11)
+        fill_range(neo,GREEN,12,15)
+        time.sleep(0.3)
+    
+        #MOVE 3
+        fill_range(neo,GREEN,0,3)
+        fill_range(neo,RED,4,7)
+        fill_range(neo,YELLOW,8,11)
+        fill_range(neo,BLUE,12,15)
+        time.sleep(0.3)
+    
+        #MOVE 4
+        fill_range(neo,BLUE,0,3)
+        fill_range(neo,GREEN,4,7)
+        fill_range(neo,RED,8,11)
+        fill_range(neo,YELLOW,12,15)
+        time.sleep(0.3)
+        
+    neo.fill(0)
+    one_in_n(neo)
+    one_in_n(neo,PINK)
+    one_in_n(neo,YELLOW,4)
+    
+    blink_all(neo,BLUE,0.5)
+    blink_all(neo,GREEN,0.5)
+    blink_all(neo,PINK,0.5)
+    sweep(neo)
+    sweep(neo,GREEN)
+    sweep(neo,BLUE)
+    sweep(neo,PINK)
+    sweep(neo,PINK,0,True)
+    sweep(neo,WHITE,YELLOW)
+    sweep(neo,WHITE,YELLOW,True)
+    sweep(neo,BLUE,YELLOW)
+    sweep(neo,PINK,YELLOW,True)
+    sweep(neo,GREEN,YELLOW)
+    sweep(neo,RED,YELLOW,True)
+    neo.fill(0)
+    neo.show()
+
+
+
+
+def npchasery_test():
+    print("Chaser YIELD test:\n")
+    for x, y in zip(y_sweep_stack_CW(neo, 0, 7, RED),y_sweep_stack(neo, 8, 15, RED)):
+        neo.show()
+        time.sleep(0.01)
+
+    for x, y in zip(y_sweep_stack(neo, 0, 7, RED),y_sweep_stack(neo, 8, 15, BLUE)):
+        neo.show()
+        time.sleep(0.01)
+
+    for a1, b2 in zip(y_fill(neo,0,7,RED),y_fill(neo,8,15,BLUE)):
+        time.sleep(0.2)
+        neo.show()
+    time.sleep(0.5)
+    
+    for a1, b2 in zip(y_fill(neo, 0,7,BLUE,WHITE),y_fill(neo, 8,15,YELLOW,GREEN)):
+        time.sleep(0.2)
+        neo.show()
+    time.sleep(0.5)
+
+    for a1, b2, c3, d4 in zip(y_fill(neo, 0,3,RED),y_fill(neo, 4,7,BLUE),y_fill(neo, 8,11,YELLOW),y_fill(neo, 12,15,GREEN)):
+        time.sleep(0.1)
+        neo.show()
+    time.sleep(0.3)
+    for a1, b2, c3, d4 in zip(y_fill(neo, 0,3,GREEN),y_fill(neo, 4,7,RED),y_fill(neo, 8,11,BLUE),y_fill(neo, 12,15,YELLOW)):
+        time.sleep(0.15)
+        neo.show()
+    time.sleep(0.3)    
+    for a1, b2, c3, d4 in zip(y_fill(neo, 0,3,YELLOW),y_fill(neo, 4,7,GREEN),y_fill(neo, 8,11,RED),y_fill(neo, 12,15,BLUE)):
+        time.sleep(0.15)
+        neo.show()
+    time.sleep(0.3)
+    for a1, b2, c3, d4 in zip(y_fill(neo, 0,3,BLUE),y_fill(neo, 4,7,YELLOW),y_fill(neo, 8,11,GREEN),y_fill(neo, 12,15,RED)):
+        time.sleep(0.15)
+        neo.show()
+    time.sleep(0.3)
+    for a1, b2, c3, d4 in zip(y_fill(neo, 0,3,RED),y_fill(neo, 4,7,BLUE),y_fill(neo, 8,11,YELLOW),y_fill(neo, 12,15,GREEN)):
+        time.sleep(0.1)
+        neo.show()
+    time.sleep(0.3)
+
+def run():
+    while True:
+        npchasery_test()
+        npchaser_test()
+
+
+run()
---
+++ npchasery.py
@@ -0,0 +1,66 @@
+#npchasery.py
+# Neopixel chaser with yield (generators)
+
+
+
+NUMPIXELS = 16
+
+def y_fill(neo, start, end, color=(255,0,0), bgcolor=0):
+  #blank out range
+  for n in range(start,end+1):
+    neo[n]=bgcolor
+  yield 'start'
+  #fill up range
+  for x in range(start,end+1):
+    neo[x]=color
+    yield x
+ 
+  #fill down range
+  for y in range(end,start-1,-1):
+    neo[y]=bgcolor
+    yield y
+
+def y_sweep_stack(neo, start=0, end=NUMPIXELS, color=(255,0,0), bgcolor=0):
+    for stackPos in range(end,start,-1):
+        print("startPos:{}".format(stackPos))
+        
+        for n in range(start,stackPos+1):
+            print("n:{}".format(n))
+            neo[n]=color;
+            yield n
+            neo[n]=bgcolor
+            yield n
+        neo[stackPos]=color        
+
+        for x in range(stackPos-1,start-1,-1):
+            print("x:{}".format(x))
+            neo[x]=color
+            yield x
+            #neo.show()
+            #time.sleep(delay)
+            neo[x]=bgcolor
+            yield x
+        neo[start]=color
+        yield x
+
+def y_sweep_stack_CW(neo, start=0, end=NUMPIXELS, color=(255,0,0), bgcolor=0):
+    for stackPos in range(start,end):
+        print("startPos:{}".format(stackPos))
+        for x in range(end,stackPos-1,-1):
+            print("x:{}".format(x))
+            neo[x]=color
+            yield x
+            neo[x]=bgcolor
+            yield x        
+
+        neo[x]=color        
+        
+        for n in range(stackPos+1,end+1):
+            print("n:{}".format(n))
+            neo[n]=color;
+            yield n
+            neo[n]=bgcolor
+            yield n
+
+        neo[end]=color
+        yield n
